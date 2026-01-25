eabdee8975189ad056ce87beac912f20082fed02
Leonardo Perassoli <leonardoperass@gmail.com>
2026-01-25 11:17:12 -0300

    feat: create reusable Icon component

---
+++ src/components/icon.tsx
@@ -0,0 +1,28 @@
+import { cva, type VariantProps } from "class-variance-authority";
+
+const IconVariants = cva("inline-block shrink-0", {
+    variants: {
+        variant: {
+            primary: "fill-gray-100"
+        },
+        size: {
+            sm: "h-5 w-5"
+        }
+    },
+    defaultVariants: {
+        variant: "primary",
+        size: "sm",
+    }
+})
+
+interface IconProps extends 
+    React.ComponentProps<"svg">,
+    VariantProps<typeof IconVariants>{
+    svg: React.FC<React.ComponentProps<"svg">>;
+}
+
+export default function Icon({svg: Svg,variant,size,className, ...props}: IconProps) {
+    return (
+        <Svg className={IconVariants({variant,size,className})} {...props}/>
+    )
+}
